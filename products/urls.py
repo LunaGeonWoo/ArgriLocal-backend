26b7aee29de5fd00e1fd827d6104ed5d5e062517
Geon-Woo <moongunwoo7019@naver.com>
2024-07-05 16:19:50 +0900

    add: slash

--- products/urls.py
+++ products/urls.py
@@ -3,5 +3,5 @@ from .views import Products, ProductDetail
 
 urlpatterns = [
     path("", Products.as_view()),
-    path("<int:id>", ProductDetail.as_view()),
+    path("<int:id>/", ProductDetail.as_view()),
 ]
